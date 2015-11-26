99032fafdb1183035131e6399882fe5cf50b78fe
刘泉 <liuq@uc888.cn>
2015-11-26 12:11:53 +0800

    add more log info into logstash output to make logstash can split log flow

--- logstash.go
+++ logstash.go
@@ -40,12 +40,38 @@ func NewLogstashAdapter(route *router.Route) (router.LogAdapter, error) {
 // Stream implements the router.LogAdapter interface.
 func (a *LogstashAdapter) Stream(logstream chan *router.Message) {
 	for m := range logstream {
+		logstashType := ""
+		logstashTags := ""
+		for _, kv := range m.Container.Config.Env {
+			kvp := strings.SplitN(kv, "=", 2)
+			if len(kvp) == 2 && kvp[0] == "LOGSTASH-TYPE" {
+				logstashType := strings.ToLower(kvp[1])
+			}
+
+			if len(kvp) == 2 && kvp[0] == "LOGSTASH-TAGS" {
+				logstashTags := strings.ToLower(kvp[1])
+			}
+		}
+
+		rancherIp := ""
+		rancherIpvalue, exists := m.Container.Config.Labels["io.rancher.container.ip"]
+		if (exists) {
+			rancherIp := rancherIpvalue
+		}
+
 		msg := LogstashMessage{
 			Message:  m.Data,
 			Name:     m.Container.Name,
 			ID:       m.Container.ID,
 			Image:    m.Container.Config.Image,
 			Hostname: m.Container.Config.Hostname,
+			LogType:  logstashType,
+			LogTags:  logstashTags,
+			HostIP:   m.Container.PortBinding.HostIP,
+			HostPort: m.Container.PortBinding.HostPort,
+			DockerIP: m.Container.NetworkSettings.IPAddress,
+			RancherIP: rancherIp
+
 		}
 		js, err := json.Marshal(msg)
 		if err != nil {
@@ -62,9 +88,16 @@ func (a *LogstashAdapter) Stream(logstream chan *router.Message) {
 
 // LogstashMessage is a simple JSON input to Logstash.
 type LogstashMessage struct {
-	Message  string `json:"message"`
-	Name     string `json:"docker.name"`
-	ID       string `json:"docker.id"`
-	Image    string `json:"docker.image"`
-	Hostname string `json:"docker.hostname"`
+	Message   string `json:"message"`
+	Name      string `json:"docker.name"`
+	ID        string `json:"docker.id"`
+	Image     string `json:"docker.image"`
+	Hostname  string `json:"docker.hostname"`
+	LogType	  string `json:"type"`
+	LogTags   string `json:"tags"`
+	HostIP    string `json:"hostip"`
+	HostPort  string `json:"hostport"`
+	DockerIP  string `json:"dockerip"`
+	RancherIP string `json:"rancherip"`
+
 }
